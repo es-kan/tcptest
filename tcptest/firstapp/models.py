f7b9e6e774ce2d69398d829367a25b27d8b8a178
Erik <erik@Mac-mini-van-The-Conditional-Pixel-2.local>
2017-02-13 11:12:33 +0100

    added items

--- tcptest/firstapp/models.py
+++ tcptest/firstapp/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    date_added = models.DateField()
+
+    def __str__(self):
+        return '%s. Price: %s' %(self.name, self.price)
