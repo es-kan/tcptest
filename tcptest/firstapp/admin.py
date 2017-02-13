3109f0500dff9ed8966f34fce38426a61928b2ae
Erik <erik@Mac-mini-van-The-Conditional-Pixel-2.local>
2017-02-13 14:59:41 +0100

    table added

--- tcptest/firstapp/admin.py
+++ tcptest/firstapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from models import Item
+from models import Item, Seller
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'date_added')
-    ordering = ('-date_added', 'price')
+    list_display = ('name', 'price', 'seller', 'date_added')
+    ordering = ('seller',)
 
 # Register your models here.
 admin.site.register(Item, ItemAdmin)
+admin.site.register(Seller)
